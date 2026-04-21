d81ce28c26f4b684a158cb91342132eac17ba21e
Divakar <vdivakar317@gmail.com>
2026-04-21 09:45:23 +0530

    remove unused CSS and stylesheets to clean up the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { AppProvider, AppContext } from './context/AppContext';
 import { AppRouter } from './router/AppRouter';
 import { useContext } from 'react';
-import './App.css';
 
 const MainApp = () => {
   return <AppRouter />;
